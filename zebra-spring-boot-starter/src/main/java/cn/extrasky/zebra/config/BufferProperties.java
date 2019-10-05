b7aa4df075a7b1902486a5315464fc83db2e4e1c
杨国栋 <gd.shunhua@gmail.com>
2019-10-05 21:51:04 +0800

    1、本地加载buffer副本后，删除副本文件，防止下次进程死掉后未生成副本文件 2、starter调整

--- zebra-spring-boot-starter/src/main/java/cn/extrasky/zebra/config/BufferProperties.java
+++ zebra-spring-boot-starter/src/main/java/cn/extrasky/zebra/config/BufferProperties.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author: 田培融
  * @date: 2019-09-25 19:48
  */
-@ConfigurationProperties(prefix = "buffer")
+//@ConfigurationProperties(prefix = "buffer")
 @Data
 public class BufferProperties {
 
